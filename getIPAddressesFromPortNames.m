566cb38c3f829eee25fe24b34316888be2961fb6
Joshua Adkinson <51720499+adkinson@users.noreply.github.com>
2024-02-23 19:58:43 -0600

    Initial commit

---
+++ getIPAddressesFromPortNames.m
@@ -0,0 +1,10 @@
+function ipAddresses = getIPAddressesFromPortNames(networkPortNames)
+
+ipAddresses = cell(size(networkPortNames));
+for i = 1:length(networkPortNames)
+    [~,out] = system(['netsh interface ip show address "',networkPortNames{i},'" | findstr "IP Address"']);
+    ipAddresses{i} = regexp(out,'[\d\.]+','match');
+end
+ipAddresses = vertcat(ipAddresses{:});
+
+end
