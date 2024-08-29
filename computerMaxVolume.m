3c66cf819baf9fa21fc824f3a57a684b3d2e0b80
Joshua Adkinson <51720499+adkinson@users.noreply.github.com>
2024-08-29 15:01:41 -0500

    Updated inline code documentation

--- computerMaxVolume.m
+++ computerMaxVolume.m
@@ -1,4 +1,15 @@
 function computerMaxVolume()
+%COMPUTERMAXVOLUME Set the volume of the computer to its maximum settings
+%
+% CODE PURPOSE
+% Set the volume of the computer to its maximum settings
+%
+% SYNTAX
+% computerMaxVolume()
+%
+% Author: Joshua Adkinson
+
 system('nircmd mutesysvolume 0');
 system('nircmd setsysvolume 65535');
+
 end
--- computerMute.m
+++ computerMute.m
@@ -1,3 +1,14 @@
 function computerMute()
+%COMPUTERMUTE Mute the computer volume
+%
+% CODE PURPOSE
+% Mute the computer volume
+%
+% SYNTAX
+% computerMute()
+%
+% Author: Joshua Adkinson
+
 system('nircmd setsysvolume 0');
+
 end
